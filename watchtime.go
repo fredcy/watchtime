17e9a401cbb91db246a25889a77f3411ab7bdf70
Fred Yankowski <fcy@app1.devnet.imsa.edu>
2014-06-26 13:13:57 -0500

    Add -wait option. Report stats in interrupt.

--- watchtime.go
+++ watchtime.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"math/rand"
+	"os"
+	"os/signal"
 	"time"
 )
 
 func main() {
-	previous := time.Now()
+	verbose := flag.Bool("verbose", false, "verbose output")
+	rate := flag.Duration("wait", 10 * time.Millisecond, "sampling interval")
+	flag.Parse()
+	start_time := time.Now()
+	previous := start_time
+	samples := 0
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			elapsed := time.Now().Sub(start_time)
+			rate := elapsed / time.Duration(samples)
+			fmt.Printf("%v: samples = %v, elapsed = %v, rate = %v\n", sig, samples, elapsed, rate)
+		}
+	}()
 	for {
-		sleeptime := time.Duration(rand.Int31n(10)) * time.Millisecond
+		sleeptime = *rate
 		time.Sleep(sleeptime)
 		now := time.Now()
 		diff := now.Sub(previous)
 		previous = now
-		//fmt.Printf("%s: diff = %s\n", now, diff)
+		samples += 1
+		if *verbose {
+			fmt.Printf("%s: diff = %s\n", now, diff)
+		}
 		if diff < 0 {
 			fmt.Printf("%s: diff = %s\n", now, diff)
 			break
